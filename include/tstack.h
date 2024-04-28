5cbe9b259eec405f9bd1d9f88591d2da5cd487e3
Rmrvn <160429541+Rmrvn@users.noreply.github.com>
2024-04-29 00:43:48 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,29 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T* stackArray;
+    int topIndex;
+ public:
+    TStack() : topIndex(-1) {
+        stackArray = new T[size];
+    }
+    void pop() {
+        if (topIndex >= 0)
+            topIndex--;
+    }
+    void push(T item) {
+        if (topIndex < size - 1)
+            stackArray[++topIndex] = item;
+    }
+    T get() const {
+        return stackArray[topIndex];
+    }
+    bool checkEmpty() const {
+        return topIndex == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
