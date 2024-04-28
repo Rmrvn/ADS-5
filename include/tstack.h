938e0a8c38c02f5af273b3e7b004b542a8ebba54
Rmrvn <160429541+Rmrvn@users.noreply.github.com>
2024-04-29 01:54:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,33 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
+#include <string>
+template<typename T, const int size>
 class TStack {
-  // добавьте код стека
-};
+    T data[100];
+    int Top;
 
+public:
+    TStack() {
+        Top = 0;
+    }
+    void PUSH(const T& item) {
+        if (Top <= size - 1)
+            data[Top++] = item;
+        else
+            throw std::string("Full!");
+    }
+    T POP() {
+        if (Top > 0)
+            return data[--Top];
+        else
+            throw std::string("Empty!");
+    }
+    T ViewTop() {
+        if (Top > 0)
+            return data[Top - 1];
+        else
+            return -1;
+    }
+};
 #endif  // INCLUDE_TSTACK_H_
